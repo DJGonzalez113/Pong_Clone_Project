7eede025f2245f84ab0999612f1bf172b298d3cd
Daniel Gonzalez <daniel.j.gonzalez54@gmail.com>
2024-02-13 23:05:59 -0600

    Player cannot move off screen

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,11 +2,14 @@ extends StaticBody2D
 
 
 const SPEED = 500
+var win_height
+var p_height
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	win_height = get_viewport_rect().size.y
+	p_height = $Sprite.texture.get_size().y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +18,5 @@ func _process(delta):
 		position.y -= SPEED * delta
 	elif Input.is_action_pressed("ui_down"):
 		position.y += SPEED * delta
+	
+	position.y = clamp(position.y, p_height/2, win_height - p_height/2)
