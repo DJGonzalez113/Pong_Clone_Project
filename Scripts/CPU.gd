dcb4e6a0fd69c00b1a0767af22bb93126b5c50c4
Daniel Gonzalez <daniel.j.gonzalez54@gmail.com>
2024-02-16 16:47:25 -0600

    Added CPU movement

--- Scripts/CPU.gd
+++ Scripts/CPU.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 
+const SPEED = 500
 var ball_pos : Vector2
 var dist
 var move_by
@@ -9,9 +10,19 @@ var paddle_h
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	window_h = get_viewport_rect().size.y
+	paddle_h = $Sprite.texture.get_size().y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	ball_pos = get_parent().get_node("Ball").position
+	dist = position.y - ball_pos.y
+	
+	if abs(dist) > SPEED * delta:
+		move_by = SPEED * delta * (dist / abs(dist))
+	else:
+		move_by = dist
+	
+	position.y -= move_by
+	
+	position.y = clamp(position.y, paddle_h/2, window_h - paddle_h/2)
