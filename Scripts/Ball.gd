8884677c961c546de55091ce813ba86c78b02559
Daniel Gonzalez <daniel.j.gonzalez54@gmail.com>
2024-02-16 17:25:13 -0600

    Added ball bounce

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 
 const INIT_SPEED = 500
-const ACCL = 10
+const ACCL = 30
+const MAX_Y_VECTOR : float = 0.6
 var window
 var speed : int
 var dir : Vector2
@@ -21,7 +22,15 @@ func new_ball():
 	dir = rand_dir()
 
 func _physics_process(delta):
-	move_and_collide(dir * speed * delta)
+	var collision = move_and_collide(dir * speed * delta)
+	var collider
+	if collision:
+		collider = collision.get_collider()
+		if collider == get_parent().get_node("Player") or collider == get_parent().get_node("CPU"):
+			speed += ACCL
+			dir = new_dir(collider)
+		else:
+			dir = dir.bounce(collision.get_normal())
 
 func rand_dir():
 	var new_dir := Vector2()
@@ -29,12 +38,19 @@ func rand_dir():
 	new_dir.x = [-1, 1][randi() % 2]
 	new_dir.y = [-0.4, 0.4][randi() % 2]
 	return new_dir.normalized()
+
+func new_dir(collider):
+	var ball_y = position.y
+	var paddle_y = collider.position.y
+	var dist = ball_y - paddle_y
+	var new_dir := Vector2()
 	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	if dir.x > 0:
+		new_dir.x = -1
+	else:
+		new_dir.x = 1
+	new_dir.y = (dist / (collider.paddle_h / 2)) * MAX_Y_VECTOR
+	return new_dir.normalized()
 
 
 func _on_BallTimer_timeout():
