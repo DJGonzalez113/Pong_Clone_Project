27920bfbcb8c806694284a16a011425fc10076bd
Daniel Gonzalez <daniel.j.gonzalez54@gmail.com>
2024-02-16 14:45:30 -0600

    goals interact with ball

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var p_score = 0
+var cpu_score = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func _process(delta):
+	$HUD/PlayerScore.text = str(p_score)
+	$HUD/CPUScore.text = str(cpu_score)
+
+func _on_LeftGoal_body_entered(body):
+	cpu_score += 1
+	$BallTimer.start()
+
+func _on_RightGoal_body_entered(body):
+	p_score += 1
+	$BallTimer.start()
